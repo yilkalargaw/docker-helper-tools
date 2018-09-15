f82bfe8f69fc409ef1e86dc037ed7d0d463ea882
lispy-dobby <yilkalargawworkneh@gmail.com>
2018-09-15 10:02:31 +0000

    change ends_with? to end_with?

--- docker_updater.rb
+++ docker_updater.rb
@@ -3,5 +3,5 @@
   .split("\n")
   .drop(1)
   .map { |i| j=i.split(' '); j[0] + ':' + j[1] }
-  .reject { |i| i.ends_with?('<none>') }
+  .reject { |i| i.end_with?('<none>') }
   .each { |i| system("docker pull #{i}") }
