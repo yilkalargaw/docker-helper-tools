4afe08b82ffb216138200287b9912e82cbe2375e
YmaY <yilkalargawworkneh@gmail.com>
2018-09-15 09:17:36 +0000

    initial commit

---
+++ docker_updater.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+`docker images`
+  .split("\n")
+  .drop(1)
+  .map { |i| j=i.split(' '); j[0] + ':' + j[1] }
+  .reject { |i| i.ends_with?('<none>') }
+  .each { |i| system("docker pull #{i}") }
